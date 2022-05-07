07469c3ebdb6668989c2921191f4487f03943a69
Ricardo Costa <ricardo18acs@gmail.com>
2022-05-07 17:41:03 -0300

    feat: time decreaser

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { MouseEvent, ReactElement, useEffect, useState } from 'react';
-import { DEFAULT_BG_ACTIVE_COLOR, DEFAULT_BG_DEACTIVE_COLOR, DEFAULT_BLOCKS_BY_LEVEL, DEFAULT_INTERVAL } from './app/configs';
+import { DEFAULT_BLOCKS_BY_LEVEL, DEFAULT_DECREASER_INTERVAL_PERCENT, DEFAULT_INTERVAL } from './app/configs';
 import { activeBlock, deactiveBlock } from './app/utils/blocks.utils';
 import './assets/styles.scss';
 
@@ -10,28 +10,31 @@ export type GameStatus = {
 };
 
 const App = () => {
-
   // STATES
-  const [ gameStatus, setGameStatus ] = useState<GameStatus>({
+  const [ gameStatus ] = useState<GameStatus>({
     theSequence: [ 3, 0, 1, 2 ],
     sequence: [],
     level: 1,
   });
-  const [ blocks, setBlocks ] = useState<number>(gameStatus.level * DEFAULT_BLOCKS_BY_LEVEL);
+  const [ blocks ] = useState<number>(gameStatus.level * DEFAULT_BLOCKS_BY_LEVEL);
+  const [ timer, setTimer ] = useState<number>(DEFAULT_INTERVAL);
 
   // HANDLE CLICK
-  const clickBlock = (event: MouseEvent, blockId: number) => {
-    console.log(event, blockId);
-  
+  const clickBlock = (event: MouseEvent, blockId: number) => {  
     gameStatus.sequence.push(Number(blockId));
-    console.log(gameStatus.sequence);
   
     if (
       gameStatus.theSequence.length === gameStatus.sequence.length &&
       gameStatus.theSequence.every((value, index) => value === gameStatus.sequence[index])
     ) {
       alert("Parabéns!!");
-      document.location.reload();
+      // document.location.reload();
+
+      // restart game, with less timer
+      setTimer(timer * DEFAULT_DECREASER_INTERVAL_PERCENT);
+      // restart game
+      restartGame();
+
     } else if (gameStatus.theSequence.length === gameStatus.sequence.length) {
       alert("Deu Ruim :[");
       document.location.reload();
@@ -49,9 +52,7 @@ const App = () => {
           onClick={(e: MouseEvent) => {
           e.preventDefault();
           clickBlock(e, block);
-        }} className="block">
-          <div className='block-cover'>{block}</div>
-        </div>
+        }} className="block"></div>
       )
     }
     return el;
@@ -59,37 +60,47 @@ const App = () => {
 
   let agente: any;
 
+  const startGame = () => {
+    let oldItem: number | undefined = undefined;
+    const sequence = [ ...gameStatus.theSequence ];
+    
+    agente = setInterval(() => {
+      if (sequence.length) {
+        if (oldItem === undefined) {
+          oldItem = sequence.shift();
+        } else {
+          deactiveBlock(oldItem);
+          oldItem = sequence.shift();
+        }
+        activeBlock(oldItem);
+      } else if (oldItem != undefined) {
+        deactiveBlock(oldItem);
+      }
+      
+    }, timer);
+  }
+
+  const restartGame = () => {
+    if (agente) clearInterval(agente);
+    gameStatus.sequence = [];
+    startGame();
+  }
+
   useEffect(() => {
       // your post layout code (or 'effect') here.
-      let oldItem: number | undefined = undefined;
-      const sequence = [ ...gameStatus.theSequence ];
-      
-      agente = setInterval(() => {
-        if (sequence.length) {
-          if (oldItem === undefined) {
-            oldItem = sequence.shift();
-          } else {
-            deactiveBlock(oldItem);
-            oldItem = sequence.shift();
-          }
-          activeBlock(oldItem);
-        } else if (oldItem != undefined) {
-          deactiveBlock(oldItem);
-        }
-        
-      }, DEFAULT_INTERVAL);
+      startGame();
     },
     // array of variables that can trigger an update if they change. Pass an
     // an empty array if you just want to run it once after component mounted. 
     []
   );
 
-  useEffect(() => {
-    return () => {
-      // Anything in here is fired on component unmount.
-      if (agente) clearInterval(agente);
-    }
-  }, [])
+  // useEffect(() => {
+  //   return () => {
+  //     // Anything in here is fired on component unmount.
+  //     if (agente) clearInterval(agente);
+  //   }
+  // }, [])
 
   return (
     <div id="main">
--- src/app/configs/index.ts
+++ src/app/configs/index.ts
@@ -1,6 +1,8 @@
 export const DEFAULT_START_SCORE = 100;
 export const DEFAULT_BLOCKS_BY_LEVEL = 4;
 
-export const DEFAULT_INTERVAL = 1500;
+export const DEFAULT_INTERVAL = 1000;
+export const DEFAULT_DECREASER_INTERVAL_PERCENT = 0.8; // -20%
+
 export const DEFAULT_BG_DEACTIVE_COLOR = "rgba(255, 255, 0, 0.39)";
 export const DEFAULT_BG_ACTIVE_COLOR = "rgba(155, 155, 37, 0.39)";
